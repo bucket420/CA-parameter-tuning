51c113e3b4db1dba39baf769f31d67d3f1620d3a
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2023-06-26 11:26:47 +0200

    Test efficiency retrieval  from output

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import subprocess
+from io import StringIO
+
 
 CAThetaCutBarrel = 0.003
 CAThetaCutForward = 0.004
@@ -12,6 +14,18 @@ output = subprocess.run(['cmsRun','step3_RAW2DIGI_RECO_VALIDATION_DQM.py',
                 f'CAThetaCutForward={CAThetaCutForward}',
                 f'dcaCutInnerTriplet={dcaCutInnerTriplet}',
                 f'dcaCutOuterTriplet={dcaCutOuterTriplet}'],
-                capture_output=True)
+                capture_output=True,
+                text=True)
 
-print(output.stdout)
+outputLines = StringIO(output)
+while line:=outputLines.readline():
+    if 'Collection' in line:
+        line = outputLines.readline()
+        line = outputLines.readline()
+        line = outputLines.readline()
+        totalRec = int(line.split()[-1])
+        line = outputLines.readline()
+        totalAss = int(line.split()[-1])
+        efficiency = totalAss / totalRec
+        print(efficiency)
+
