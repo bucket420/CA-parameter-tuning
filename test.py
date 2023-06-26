440aeb66264f936b317e9060ec6762df6c5dfab1
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2023-06-26 11:08:19 +0200

    Print process output

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 import subprocess
 
 CAThetaCutBarrel = 0.003
-subprocess.run(['cmsRun','step3_RAW2DIGI_RECO_VALIDATION_DQM.py', f'CAThetaCutBarrel={CAThetaCutBarrel}'])
+CAThetaCutForward = 0.004
+dcaCutInnerTriplet = 0.16
+dcaCutOuterTriplet = 0.26
+
+#process
+
+output = subprocess.run(['cmsRun','step3_RAW2DIGI_RECO_VALIDATION_DQM.py', 
+                f'CAThetaCutBarrel={CAThetaCutBarrel}',
+                f'CAThetaCutForward={CAThetaCutForward}',
+                f'dcaCutInnerTriplet={dcaCutInnerTriplet}',
+                f'dcaCutOuterTriplet={dcaCutOuterTriplet}'],
+                capture_output=True)
+
+print(output.stdout)
