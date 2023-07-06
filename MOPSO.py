39b5821b66fe8d0e8e6fe776c2458af6de858298
Chris Hoang <hoadh-23@rhodes.edu>
2023-07-06 14:28:03 +0200

    save current state

--- MOPSO.py
+++ MOPSO.py
@@ -1,17 +1,25 @@
 import numpy as np
 import subprocess
 import os
-from utils import get_metrics, write_csv
+from utils import get_metrics, write_csv, read_csv
 import uproot
 import json
 
 class Particle:
-    def __init__(self, lb=-10, ub=10, num_objectives=2):
-        self.position = np.random.uniform(lb, ub)
-        self.velocity = np.zeros_like(self.position)
-        self.best_position = self.position
-        self.best_fitness = np.ones(num_objectives) #inf for minimization
-        self.fitness = np.ones(num_objectives)
+    def __init__(self, lb=-10, ub=10, num_objectives=2, velocity=None, position=None, 
+                 fitness=None, best_position=None, best_fitness=None):
+        if all([velocity, position, fitness, best_position, best_fitness]):
+            self.velocity = velocity
+            self.position = position
+            self.fitness = fitness
+            self.best_position = best_position
+            self.best_fitness = best_fitness
+        else:
+            self.position = np.random.uniform(lb, ub)
+            self.velocity = np.zeros_like(self.position)
+            self.best_position = self.position
+            self.best_fitness = np.ones(num_objectives) #inf for minimization
+            self.fitness = np.ones(num_objectives)
 
     def update_velocity(self, global_best_position, w=0.5, c1=1, c2=1):
         r1 = np.random.uniform(0, 1)
@@ -38,6 +46,7 @@ class PSO:
     def __init__(self, lb, ub, num_objectives=2, num_particles=50, w=0.5, c1=1, c2=1, 
                  num_iterations=100, state_json=None, max_iter_no_improv=None, tol=None):
         if not state_json:
+            self.is_new_run = True
             self.num_particles = num_particles
             self.lb = lb
             self.ub = ub
@@ -51,48 +60,94 @@ class PSO:
             self.particles = [Particle(lb, ub) for _ in range(num_particles)]
             self.global_best_position = np.zeros_like(lb)
             self.global_best_fitness = np.ones(num_objectives)
-            write_csv('parameters.csv', [self.particles[i].position for i in range(self.num_particles)]) 
-            json.dumps({
+            self.state = {
                 "num_objectives": self.num_objectives,
                 "num_particles": self.num_particles,
-                "num_iterations": self.num_iterations
-                
-                
-            })
+                "num_iterations": self.num_iterations,
+                "max_iter_no_improv": self.max_iter_no_improv,
+                "tol": self.tol,
+                "lb": self.lb,
+                "ub": self.ub,
+                "w": self.w,
+                "c1": self.c1,
+                "c2": self.c2,
+                "global_best_position": self.global_best_position,
+                "global_best_fitness": self.global_best_fitness,
+                "iteration": 0
+            }
+            write_csv('parameters.csv', [self.particles[i].position for i in range(self.num_particles)])
+            with open('pso_state.json', 'w') as f:
+                json.dump(self.state, f)
         else:
-            pass
+            with open(state_json) as f:
+                self.state = json.load(f)
+            self.is_new_run = False
+            self.num_particles = self.state["num_particles"]
+            self.lb = self.state["lb"]
+            self.ub = self.state["ub"]
+            self.w = self.state["w"]
+            self.c1 = self.state["c1"]
+            self.c2 = self.state["c2"]
+            self.num_iterations = self.state["num_iterations"]
+            self.num_objectives = self.state["num_objectives"]
+            self.max_iter_no_improv = self.state["max_iter_no_improv"]
+            self.tol = self.state["tol"]
+            self.global_best_fitness = np.array(self.state["global_best_fitness"], astype=float)
+            self.global_best_position = np.array(self.state["global_best_position"], astype=float)
+            num_params = len(self.lb)
+            particles_state = read_csv('history/particles_state.csv')
+            self.particles = [Particle(
+                                lb=self.lb, 
+                                ub=self.ub, 
+                                num_objectives=self.num_objectives,
+                                position=np.array(particles_state[i][:num_params], dtype=float),
+                                velocity=np.array(particles_state[i][num_params:2*num_params], dtype=float),
+                                best_position=np.array(particles_state[i][2*num_params:3*num_params], dtype=float),
+                                fitness=np.array(particles_state[i][3*num_params:3*num_params+num_objectives], dtype=float),
+                                best_fitness=np.array(particles_state[i][3*num_params+num_objectives:], dtype=float)
+                             ) for i in range(self.num_particles)]
             
-            
-  
 
     def optimize(self):
         uproot_file = None
         # clear old data, probably not the best way to do this
-        os.system("rm -rf history/parameters/*")
-        os.system("rm -rf history/validation/*")
-        os.system("rm -rf history/pareto_front/*")
-        for i in range(self.num_iterations):
-            write_csv('history/parameters/iteration' + str(i) + '.csv', [self.particles[i].position for i in range(self.num_particles)])
-            validation_result = "history/validation/iteration" + str(i) + ".root"
-            subprocess.run(['cmsRun','reconstruction.py', "outputFileName=" + validation_result])
+        if not self.is_new_run:
+            os.system("rm -rf history/parameters/*")
+            os.system("rm -rf history/validation/*")
+            os.system("rm -rf history/pareto_front/*")
             
-            for j, particle in enumerate(self.particles):
-                uproot_file = uproot.open(validation_result)
-                particle.evaluate_fitness(uproot_file, j)
+        for i in range(self.num_iterations):
+            if self.is_new_run:
+                write_csv('history/parameters/iteration' + str(self.state["iteration"]) + '.csv', [self.particles[i].position for i in range(self.num_particles)])
+                validation_result = "history/validation/iteration" + str(self.state["iteration"]) + ".root"
+                subprocess.run(['cmsRun','reconstruction.py', "outputFileName=" + validation_result])
+                
+                for j, particle in enumerate(self.particles):
+                    uproot_file = uproot.open(validation_result)
+                    particle.evaluate_fitness(uproot_file, j)
+                    if all(particle.fitness < self.global_best_fitness): 
+                        self.global_best_fitness = particle.fitness
+                        self.global_best_position = particle.position
+                uproot_file.close()
                 
             pareto_front = self.get_pareto_front()
-            write_csv('history/pareto_front/iteration' + str(i) + '.csv', 
-                      [np.concatenate([pareto_front[i].position, pareto_front[i].fitness]) for i in range(len(pareto_front))])
+            write_csv('history/pareto_front/iteration' + str(self.state["iteration"]) + '.csv', 
+                      [np.concatenate([pareto_front[self.state["iteration"]].position, pareto_front[i].fitness]) for i in range(len(pareto_front))])
+            
+            write_csv('history/particles_state.csv', 
+                      [np.concatenate([particle.position, particle.velocity, particle.best_position, particle.fitness, particle.best_fitness]) 
+                       for particle in self.particles])
+            
+            self.state["iteration"] += 1
+            self.state["global_best_position"] = self.global_best_position.tolist()
+            self.state["global_best_fitness"] = self.global_best_fitness.tolist()
+            with open('pso_state.json', 'w') as f:
+                json.dump(self.state, f)    
 
             for j, particle in enumerate(self.particles):
-                if all(particle.fitness < self.global_best_fitness): 
-                    self.global_best_fitness = particle.fitness
-                    self.global_best_position = particle.position
-
                 particle.update_velocity(self.global_best_position, self.w, self.c1, self.c2)
                 particle.update_position(self.lb, self.ub)
                 
-            uproot_file.close()
             write_csv('parameters.csv', [self.particles[i].position for i in range(self.num_particles)])
 
     def get_pareto_front(self):
