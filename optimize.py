d5c66bb14d1f6373b6213b6ee465c9cd4a07181c
bucket420 <hoadh-23@rhodes.edu>
2023-07-17 10:39:18 +0200

    fix at counter

--- optimize.py
+++ optimize.py
@@ -11,7 +11,7 @@ lb = [0.0, 0.0, 0.0, 0.0] #!!!
 ub = [0.006, 0.03, 0.2, 1.0] #!!!
 
 # create the PSO object
-pso = PSO(lb=lb, ub=ub, num_particles=200, num_iterations=80, w=0.5, 
+pso = PSO(lb=lb, ub=ub, num_particles=200, num_iterations=10, w=0.5, 
           c1=1, c2=1, max_iter_no_improv=None, tol=None, continuing=args.continuing)
 
 # run the optimization algorithm
